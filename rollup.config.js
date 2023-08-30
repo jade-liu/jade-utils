25968f806b240f6c16d09808fd76bacf02826a35
liuyu <2259023634@qq.com>
2023-08-30 15:11:29 +0800

    打包名称修改

--- rollup.config.js
+++ rollup.config.js
@@ -5,18 +5,18 @@ module.exports = {
 	output: [
 		{
 			format: 'cjs', // 打包为commonjs格式
-			file: 'dist/dip-utils.cjs.js', // 打包后的文件路径名称
-			name: 'dutils' // 打包后的默认导出文件名称
+			file: 'dist/jade-utils.cjs.js', // 打包后的文件路径名称
+			name: 'jutils' // 打包后的默认导出文件名称
 		},
 		{
 			format: 'esm', // 打包为esm格式
-			file: 'dist/dip-utils.esm.js',
-			name: 'dutils'
+			file: 'dist/jade-utils.esm.js',
+			name: 'jutils'
 		},
 		{
 			format: 'umd', // 打包为umd通用格式
-			file: 'dist/dip-utils.umd.js',
-			name: 'dutils',
+			file: 'dist/jade-utils.umd.js',
+			name: 'jutils',
 			minifyInternalExports: true
 		}
 	],
